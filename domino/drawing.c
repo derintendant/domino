be59bcdfdfd029ed2222b3b4630222745c4a7c7e
Dennis Stengele <d.stengele@derintendant.de>
2013-04-06 15:26:51 +0200

    Added drawing function, initBoard() to initialize the Board and drawDomino() do draw a domino either horizontal or vertical

---
+++ domino/drawing.c
@@ -0,0 +1,147 @@
+//
+//  drawing.c
+//  domino
+//
+//  Created by Dennis Stengele on 02.04.13.
+//  Copyright (c) 2013 Dennis Stengele. All rights reserved.
+//
+																		
+
+#include <stdio.h>
+#include "drawing.h"
+#include "macros.h"
+
+void initBoard(void) {
+	CLS;
+	for (int i = 1; i <= 17; i++) {
+		GOTO(i, 10);
+		COLOR(1);
+		printf("#");
+	}
+	for (int i = 1; i <= 17; i++) {
+		GOTO(i, 40);
+		COLOR(1);
+		printf("#");
+	}
+	for (int i = 11; i < 40; i++) {
+		GOTO(1, i);
+		COLOR(1);
+		printf("#");
+	}
+	for (int i = 11; i < 40; i++) {
+		GOTO(17, i);
+		COLOR(1);
+		printf("#");
+	}
+	printf("\n");
+}
+
+void drawDomino(int left, int right, int orientation) {
+	if (orientation == 0) {
+		switch (left) {
+			case 0:
+				printf("\u2610");
+				break;
+			case 1:
+				printf("\u2680");
+				break;
+			case 2:
+				printf("\u2681");
+				break;
+			case 3:
+				printf("\u2682");
+				break;
+			case 4:
+				printf("\u2683");
+				break;
+			case 5:
+				printf("\u2684");
+				break;
+			case 6:
+				printf("\u2685");
+				break;
+			default:
+				break;
+		}
+		switch (right) {
+			case 0:
+				printf("\u2610");
+				break;
+			case 1:
+				printf("\u2680");
+				break;
+			case 2:
+				printf("\u2681");
+				break;
+			case 3:
+				printf("\u2682");
+				break;
+			case 4:
+				printf("\u2683");
+				break;
+			case 5:
+				printf("\u2684");
+				break;
+			case 6:
+				printf("\u2685");
+				break;
+			default:
+				break;
+		}
+	}
+	
+	if (orientation == 1) {
+		switch (left) {
+			case 0:
+				printf("\u2610");
+				break;
+			case 1:
+				printf("\u2680");
+				break;
+			case 2:
+				printf("\u2681");
+				break;
+			case 3:
+				printf("\u2682");
+				break;
+			case 4:
+				printf("\u2683");
+				break;
+			case 5:
+				printf("\u2684");
+				break;
+			case 6:
+				printf("\u2685");
+				break;
+			default:
+				break;
+		}
+		CURDOWN(1);
+		CURRIGHT(1);
+		switch (right) {
+			case 0:
+				printf("\u2610");
+				break;
+			case 1:
+				printf("\u2680");
+				break;
+			case 2:
+				printf("\u2681");
+				break;
+			case 3:
+				printf("\u2682");
+				break;
+			case 4:
+				printf("\u2683");
+				break;
+			case 5:
+				printf("\u2684");
+				break;
+			case 6:
+				printf("\u2685");
+				break;
+			default:
+				break;
+		}
+	}
+}
---
+++ domino/drawing.h
@@ -0,0 +1,15 @@
+//
+//  drawing.h
+//  domino
+//
+//  Created by Dennis Stengele on 06.04.13.
+//  Copyright (c) 2013 Dennis Stengele. All rights reserved.
+//
+
+#ifndef domino_drawing_h
+#define domino_drawing_h
+
+void initBoard(void);
+void drawDomino(int, int, int);
+
+#endif
